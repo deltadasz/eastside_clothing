071c89a422ee932fbc5a4dc184633e253f6faa14
deltadasz <58606011+deltadasz@users.noreply.github.com>
2022-06-21 14:05:49 +0300

    redux

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,12 +3,15 @@ import Button from '../button/button.component';
 import {CartDropdownContainer, EmptyMessage, CartItems} from  './cart-dropdown.styles';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
+
 import { useNavigate } from 'react-router-dom';
 
 
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
+
+    
     const navigate = useNavigate();
 
     const goToCheckoutHandler = () => {
