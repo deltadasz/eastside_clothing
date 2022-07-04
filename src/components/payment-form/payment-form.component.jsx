a5193b7cd34d5bff8dd9f0102c8a99c117b4c654
deltadasz <58606011+deltadasz@users.noreply.github.com>
2022-07-04 19:16:03 +0300

    final v3

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,57 +13,59 @@ const PaymentForm = () => {
     const elements = useElements();
     const amount = useSelector(selectCartTotal);
     const currentUser = useSelector(selectCurrentUser);
-    const [isProcessingPayment, setIsProcessingPayment] = useState(false);   
-    
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  
     const paymentHandler = async (e) => {
-        e.preventDefault();
-
-        if (!stripe || !elements) {
-            return;
+      e.preventDefault();
+      if (!stripe || !elements) {
+        return;
+      }
+      setIsProcessingPayment(true);
+      const response = await fetch('/.netlify/functions/create-payment-intent', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: amount * 100 }),
+      }).then((res) => {
+        return res.json();
+      });
+  
+      const clientSecret = response.paymentIntent.client_secret;
+  
+      const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: currentUser ? currentUser.displayName : 'Guest',
+          },
+        },
+      });
+  
+      setIsProcessingPayment(false);
+  
+      if (paymentResult.error) {
+        alert(paymentResult.error.message);
+      } else {
+        if (paymentResult.paymentIntent.status === 'succeeded') {
+          alert('Plata a fost realizată cu succes!');
         }
-
-        setIsProcessingPayment(true);
-
-        const response = await fetch ('/.netlify/functions/create-payment-intent', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({amount: amount * 100}),
-        }).then(res => res.json());
-        
-        const {paymentIntent: {client_secret}} = response;
-
-
-        const paymentResult = await stripe.confirmCardPayment(client_secret, {
-            payment_method: {
-                card: elements.getElement(CardElement),
-                billing_details: {
-                    name:currentUser ? currentUser.displayName : 'Guest',
-                },
-            },
-        });
-
-        setIsProcessingPayment(false);
-
-        if(paymentResult.error) {
-            alert(paymentResult.error);
-        }else {
-            if(paymentResult.paymentIntent.status === "succeeded") {
-                alert('Payment Successful');
-            }
-        }
+      }
     };
-
+  
     return (
-        <PaymentFormContainer>
-            <FormContainer onSubmit={paymentHandler}>
-            <h2>Detalii card:</h2>
-            <CardElement/>
-            <PaymentButton isLoading={isProcessingPayment} buttonType={BUTTON_TYPE_CLASSES.inverted}>Plătește acum</PaymentButton>
-            </FormContainer>
-         </PaymentFormContainer>
+      <PaymentFormContainer>
+        <FormContainer onSubmit={paymentHandler}>
+          <h2>Credit Card Payment:</h2>
+          <CardElement />
+          <PaymentButton
+            buttonType={BUTTON_TYPE_CLASSES.inverted}
+            isLoading={isProcessingPayment}
+          >
+            Pay Now
+          </PaymentButton>
+        </FormContainer>
+      </PaymentFormContainer>
     );
-};
-
-export default PaymentForm;
+  };
+  export default PaymentForm;
